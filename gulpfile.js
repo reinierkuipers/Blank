59455b842ad849e0ea69fb4aa117085ffb59abd6
Reinier Kuipers <reinierkuipers@Reiniers-MacBook-Pro.local>
2019-01-08 14:52:58 +0100

    Updated project

--- gulpfile.js
+++ gulpfile.js
@@ -1,14 +1,12 @@
 // VARIABLES
 
 var gulp            = require('gulp');
-var uglify          = require('gulp-uglify');
-var concat          = require('gulp-concat');
 var sass            = require('gulp-sass');
-var sourcemaps      = require('gulp-sourcemaps');
-var cleanCSS        = require('gulp-clean-css');
-var minifyCSS       = require('gulp-minify-css');
-var purgeSourcemaps = require('gulp-purge-sourcemaps');
+var runSequence     = require('run-sequence');
+var autoprefixer    = require('gulp-autoprefixer');
 var browserSync     = require('browser-sync').create();
+var gulpif          = require('gulp-if');
+var runSequence     = require('run-sequence');
 var autoreload      = browserSync.reload;
 
 const folders = {
@@ -19,15 +17,11 @@ const folders = {
 var paths = {
     styles: {
         src: './templates/' + folders.name + '/scss',
-        files: './templates/' + folders.name + '/scss/**/*.scss',
-        dest: './templates/' + folders.name + '/css'
-    },
-    scripts: {
-        src: './templates/' + folders.name + '/js',
         files: [
-            './templates/' + folders.name + '/js/vendor/*.js',
-            './templates/' + folders.name + '/assets/js/*.js',
+            '/node_modules/normalize.css/normalize.css',
+            '/templates/' + folders.name + '/scss/**/*.scss',
         ],
+        dest: './templates/' + folders.name + '/css'
     }
 };
 
@@ -35,64 +29,39 @@ var paths = {
 
 gulp.task('browser-sync', ['sass'], function() {
     browserSync.init({
-      proxy: 'http://cashquiz.localhost'
+      proxy: 'http://' + folders.name + '.localhost',
     });
 });
 
 // WATCH
 
-gulp.task('watch', function(){
-  gulp.watch('js/**/*.js',['js']);
-  gulp.watch('css/**/*.scss',['sass']);
-  gulp.watch('css/**/*.css',['css']);
-});
-
-// FILES
-
-gulp.task('files', function() {
-    gulp.src('node_modules/normalize.css/normalize.css')
-        .pipe(gulp.dest('css'));
-});
-
-// JAVASCRIPT
-
-gulp.task('js', function () {
-  return gulp.src(paths.scripts.files)
-    .pipe(uglify())
-    .pipe(concat('app.js'))
-    .pipe(gulp.dest('build'));
+gulp.task('watch', function() {
+  gulp.watch(paths.styles.files, ['sass']);
 });
 
 // SASS
 
 gulp.task('sass', function () {
-  gulp.src(paths.styles.files)
-    .pipe(sourcemaps.init())
-    .pipe(sass().on('error', sass.logError))
-    .pipe(autoprefixer({
-      browsers: ['last 2 versions'],
-      cascade: false
-    }))
-    .pipe(gulp.dest(paths.styles.dest))
-    .pipe(purgeSourcemaps())
-    .pipe(minifyCSS({keepSpecialComments:0, processImport: false}))
-    .pipe(rename({suffix: '.min'}))
+    gulp.src(paths.styles.files, { base: '.' })
+        .pipe(sass({
+            sourceComments: true,
+            outputStyle: 'expanded',
+            errLogToConsole: true
+        }).on('error', sass.logError))
+        .pipe(autoprefixer({
+            browsers: ['last 2 versions'],
+            cascade: false
+        }))
     .pipe(gulp.dest(paths.styles.dest))
     .pipe(gulpif(autoreload,browserSync.stream({match:"**/*.css"})));
 });
 
-// CSS
+// DEFAULT & WATCH
 
-gulp.task('css', function () {
-  gulp.src([
-    'css/normalize.css',
-    'css/template.css'
-    ])
-    .pipe(cleanCSS())
-    .pipe(concat('style.css'))
-    .pipe(gulp.dest('build'));
+gulp.task('watch', function() {
+  gulp.watch(paths.styles.files, ['sass']);
 });
 
-// DEFAULT
-
-gulp.task('default', ['files','js','sass','css','watch']);
+gulp.task('default', function(callback) {
+  runSequence(['sass', 'browser-sync'], 'watch', callback)
+});
